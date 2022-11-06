a4c9f02a0e57390dec2f010b647c1456d2077908
Qifan Zhang <qxz180010@utdallas.edu>
2022-11-05 21:20:09 -0500

    init_clean

--- Data_collection/create_boundingbox.sh
+++
@@ -1,4 +0,0 @@
-for ((i=1; i<=6; i++))
-do
-    python3 read_camerain_info.py --folder "20221007_5-$i" --save_folder "5-$i"
-done
--- Data_collection/create_handcenter.sh
+++
@@ -1,5 +0,0 @@
-for ((i=1; i<=6; i++))
-do
-    python3 read_sync_info.py --folder "20221007_5-$i"
-    python3 read_sync_info_left.py --folder "20221007_5-$i"
-done
